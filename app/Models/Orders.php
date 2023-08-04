07bb8a002a443b1a0604f29fc7ebce482e30a729
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-04 08:54:27 +0800

    menambahkan fungsi relasi product,users,shops di  model Orders

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -11,4 +11,16 @@ class Orders extends Model
 
     protected $table = 'orders';
     protected $guarded = ['id_order'];
+
+    public function product(){
+        return $this->hasOne(Product::class, 'product_id', 'id_product');
+    }
+
+    public function users(){
+        return $this->hasOne(User::class, 'buyer_id', 'id_user');
+    }
+
+    public function shops(){
+        return $this->hasOne(Shops::class, 'shop_id', 'id_shop');
+    }
 }
