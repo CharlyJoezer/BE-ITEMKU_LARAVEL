09b94c7a4c9f6459b4bb2e87dd46fb9e8fc8fc89
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-07 09:01:42 +0800

    Membuat model types_sub_categories

---
+++ app/Models/Types_Sub_Categories.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Types_Sub_Categories extends Model
+{
+    use HasFactory;
+
+    protected $table = 'type_sub_categories';
+    protected $guarded = ['id_type_sub_category'];
+}
