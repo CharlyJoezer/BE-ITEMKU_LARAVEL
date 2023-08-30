91dc7061f3ced3cabf1661a71fa965a41b886823
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-30 21:18:22 +0800

    Menambahkan column path_image_user di migrasi users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('path_image_user')->default('/assets/users/image/default.jpg');
             $table->timestamps();
         });
     }
